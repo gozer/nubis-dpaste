649138a75a7c59c07f130e95b2d2147bb5c31463
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-02-26 21:20:39 -0500

    add a bunch of consul outputs for confd

--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -22,6 +22,48 @@ resource "consul_keys" "dpaste" {
     }
 }
 
+# Consul config outputs
+resource "consul_keys" "app_config" {
+    datacenter = "${var.region}"
+
+    key {
+        name  = "db_name"
+	path  = "${var.project}/${var.environment}/config/db_name"
+	value = "${var.project}"
+    }
+    key {
+        name  = "db_username"
+	path  = "${var.project}/${var.environment}/config/db_username"
+	value = "${var.project}"
+    }
+    key {
+        name  = "app_db_server"
+	path  = "${var.project}/${var.environment}/config/app_db_server"
+	value = "localhost"
+    }
+
+    #XXX: Needs to be auto-generated
+    key {
+        name  = "db_password"
+	path  = "${var.project}/${var.environment}/config/db_password"
+	value = "cinwaSweu"
+    }
+
+    #XXX: Needs to be auto-generated
+    key {
+        name  = "db_root_password"
+	path  = "${var.project}/${var.environment}/config/db_root_password"
+	value = "TygwievNu"
+    }
+
+    #XXX: Needs to be auto-generated
+    key {
+        name  = "app_secret_key"
+	path  = "${var.project}/${var.environment}/config/app_secret_key"
+	value = "a88385f6-0b75-4d46-a420-4427297f52d5"
+    }
+}
+
 # Configure the AWS Provider
 provider "aws" {
     access_key = "${var.aws_access_key}"
