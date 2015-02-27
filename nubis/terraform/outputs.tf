36e57eddec89828d7839e74d2f3d5d8295c85e41
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-02-27 11:29:51 -0500

    improve output

--- nubis/terraform/outputs.tf
+++ nubis/terraform/outputs.tf
@@ -2,5 +2,5 @@ output "url" {
     value = "${aws_elb.dpaste.dns_name}"
 }
 output "instance" {
-    value = "ubuntu@${aws_instance.dpaste.public_dns}"
+    value = "${var.ssh_user}@${aws_instance.dpaste.public_dns}"
 }
