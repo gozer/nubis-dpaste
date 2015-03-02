184ecd51f3518460d9ce1952ae9139409de7edcb
Philippe M. Chiasson <gozer@ectoplasm.org>
2015-03-02 14:51:28 -0500

    better output url dualstack

--- nubis/terraform/outputs.tf
+++ nubis/terraform/outputs.tf
@@ -1,5 +1,5 @@
 output "url" {
-    value = "${aws_elb.dpaste.dns_name}"
+    value = "dualstack.${aws_elb.dpaste.dns_name}"
 }
 output "instance" {
     value = "${var.ssh_user}@${aws_instance.dpaste.public_dns}"
