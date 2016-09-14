3de7bf6417eba43d12badb54f549c9eec09c1be1
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-14 15:05:27 -0400

    Add database, autoscaling and Consul goodness

---
+++ nubis/terraform/consul.tf
@@ -0,0 +1,49 @@
+# Discover Consul settings
+module "consul" {
+  source       = "github.com/nubisproject/nubis-terraform//consul?ref=master"
+  region       = "${var.region}"
+  environment  = "${var.environment}"
+  account      = "${var.account}"
+  service_name = "${var.service_name}"
+}
+
+# Configure our Consul provider, module can't do it for us
+provider "consul" {
+  address    = "${module.consul.address}"
+  scheme     = "${module.consul.scheme}"
+  datacenter = "${module.consul.datacenter}"
+}
+
+# Publish our outputs into Consul for our application to consume
+resource "consul_keys" "config" {
+  # This one, the application generates, we only read it
+  key {
+    name = "app_secret_key"
+    path = "${module.consul.config_prefix}/APP_SECRET_KEY"
+  }
+
+  # The rest, we publish to
+  key {
+    name  = "db_name"
+    path  = "${module.consul.config_prefix}/DB_NAME"
+    value = "${module.database.name}"
+  }
+
+  key {
+    name  = "db_server"
+    path  = "${module.consul.config_prefix}/DB_SERVER"
+    value = "${module.database.address}"
+  }
+
+  key {
+    name  = "db_username"
+    path  = "${module.consul.config_prefix}/DB_USERNAME"
+    value = "${module.database.username}"
+  }
+
+  key {
+    name  = "db_password"
+    path  = "${module.consul.config_prefix}/DB_PASSWORD"
+    value = "${module.database.password}"
+  }
+}
--- nubis/terraform/main.tf
+++ nubis/terraform/main.tf
@@ -6,6 +6,13 @@ module "worker" {
   service_name = "${var.service_name}"
   ami          = "${var.ami}"
   elb          = "${module.load_balancer.name}"
+
+  # CPU utilisation based autoscaling (with good defaults)
+  scale_load_defaults = true
+
+  # Explicitely pick our load limits for up/down scaling
+  #scale_up_load = 75
+  #scale_down_load = 10
 }
 
 module "load_balancer" {
@@ -14,6 +21,15 @@ module "load_balancer" {
   environment  = "${var.environment}"
   account      = "${var.account}"
   service_name = "${var.service_name}"
+}
+
+module "database" {
+  source                 = "github.com/nubisproject/nubis-terraform//database?ref=master"
+  region                 = "${var.region}"
+  environment            = "${var.environment}"
+  account                = "${var.account}"
+  service_name           = "${var.service_name}"
+  client_security_groups = "${module.worker.security_group}"
 }
 
 module "dns" {
