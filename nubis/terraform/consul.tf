b004b4a5847f00ff9eae737e36c435036e973b45
Philippe M. Chiasson <gozer@ectoplasm.org>
2016-09-23 17:10:58 -0400

    Delete Consul keys on deployment destruction

--- nubis/terraform/consul.tf
+++ nubis/terraform/consul.tf
@@ -27,23 +27,27 @@ resource "consul_keys" "config" {
     name  = "db_name"
     path  = "${module.consul.config_prefix}/DB_NAME"
     value = "${module.database.name}"
+    delete = true
   }
 
   key {
     name  = "db_server"
     path  = "${module.consul.config_prefix}/DB_SERVER"
     value = "${module.database.address}"
+    delete = true
   }
 
   key {
     name  = "db_username"
     path  = "${module.consul.config_prefix}/DB_USERNAME"
     value = "${module.database.username}"
+    delete = true
   }
 
   key {
     name  = "db_password"
     path  = "${module.consul.config_prefix}/DB_PASSWORD"
     value = "${module.database.password}"
+    delete = true
   }
 }
